662f8e327edbc58ac8eab831d124d0e636e01477
Victor Campos <campos.viictor@gmail.com>
2023-10-09 12:49:26 -0300

    update one of the model files

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,7 +8,7 @@ protocol CoinManagerDelegate {
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "12C29A7D-1817-4238-9575-8D8D9FF6931A" // have to remove it before sending to GitHub
+    let apiKey = "YOUR-API-KEY"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
